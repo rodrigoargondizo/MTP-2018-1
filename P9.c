3625ae2ab261074dbc14fc3f91d685be4227c835
Rodrigo Faria Argondizo <37841192+rodrigoargondizo@users.noreply.github.com>
2018-07-11 13:42:35 -0300

    Update P9.c

--- P9.c
+++ P9.c
@@ -1,94 +1,92 @@
-//Nome: RODRIGO FARIA ARGONDIZO
-//MATRICULA: 11721EAU012
+//Rodrigo Faria Argondizo
+// Matricula: 11721EAU012
 
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct dimensoes{
-	float largura;
-	float profundidade;
-	float altura;
-};
+typedef struct Dimensoes{
+    float largura;
+    float profundidade;
+    float altura;
+}Dimensoes;
 
-struct produto{
-	char numero[101];
-	char nome[64];
-	float preco;
-	float dimensoes;
-	struct dimensoes end;
-};
+typedef struct Produto{
+    char nome[64];
+    float preco;
+    Dimensoes dimensoes;
+}Produto;
 
-struct produto produtos[100];
-
-void cadastrar(){
-	struct produto;
-	int i = 0;
-	printf("\n\nNumero do produto a ser cadastrado: ");
-		scanf("%s",produtos[i].numero);
-	printf("\nNome: ");
-		scanf("%s",produtos[i].nome);
-		getchar();
-	printf("\nDigite o preco: ");
-		scanf("%f", &produtos[i].preco);
-		getchar();
-	printf("\nDigite a largura: ");
-		scanf("%f", &produtos[i].end.largura);
-		getchar();
-	printf("\nDigite a produndidade: ");
-		scanf("%f", &produtos[i].end.profundidade);
-		getchar();
-	printf("\nDigite a altura: ");
-		scanf("%f", &produtos[i].end.altura);
-	printf("\n\nProduto %s cadastrado com sucesso!\n\n", produtos[i].numero);
+void cadastra(Produto * p){
+    printf("\nDigite o nome do produto: ");
+    fgets(p->nome, 64, stdin);
+    p->nome[strlen(p->nome)-1] = '\0';
+    printf("\nDigite o preco: ");
+    scanf("%f", &(p->preco));
+    getchar();
+    printf("\nDigite a largura: ");
+    scanf("%f", &(p->dimensoes.largura));
+    getchar();
+    printf("\nDigite a profundidade: ");
+    scanf("%f", &(p->dimensoes.profundidade));
+    getchar();
+    printf("\nDigite a altura: ");
+    scanf("%f", &(p->dimensoes.altura));
+    getchar();
 }
 
-void consultar(){
-	char numero_consulta[101];
-	int i = 0, flag = 0;
-	printf("\n\nDigite o numero do produto: ");
-		fflush(stdin);
-		gets(numero_consulta);
-	
-	for(i = 0 ; i < 101; i++){	
-		if(strcmp(numero_consulta, produtos[i].numero) == 0){
-			flag = 1;
-			printf("\n\n%s, R$ %.2f, ",produtos[i].nome, produtos[i].preco);
-			printf("L: %.2fm x P: %.2fm x A: %.2fm\n\n", produtos[i].end.largura, 				
-			produtos[i].end.profundidade, produtos[i].end.altura);
-			break;
-		}
-	}
-	if(flag == 0){
-		printf("\n\nProduto nao cadastrado.\n\n");
-		return;
-	}
+void consulta(Produto * p){
+    char c;
+    if(strlen(p->nome) == 0){
+        printf("\nProduto nao cadastrado!\n");
+        while((c=getchar()) != '\n');
+        return;
+    }
+    printf("\n%s, R$ %.2f, L: %.2fm x P: %.2fm x A: %.2fm\n", p->nome, p->preco,(p->dimensoes.largura),(p->dimensoes.profundidade), (p->dimensoes.altura));
+    while((c=getchar())!='\n');
 }
 
+int main(){
+    int opt1=0, opt2=0;
+    Produto * produtos = calloc(2, sizeof(Produto));
 
-int main (){
-
-	int opcao;
-	int i = 0;
-
-	struct produto produtos;
-	
-	do{
-	printf("1-Cadastrar\n2-Consultar\n3-Sair\n");
-	scanf("%d", &opcao);
-	getchar();
-		switch(opcao){
-			case 1:
-				cadastrar();
-				break;
-			case 2:
-				consultar();
-				break;
-			case 3:
-				return 0;
-		}
-	}while (opcao != 4);
-
-	return 0;	
+    char sub[2][16] = {"Cadastro", "Consulta"};
+    while(opt1 != 3){
+        printf("\n=-=-=-=-=MENU=-=-=-=-=\n");
+        printf("\n1 - Cadastrar\n2 - Consulta\n3 - Sair\n> ");
+        scanf("%d", &opt1);
+        getchar();
+        opt2 = 0;
+        switch(opt1){
+            case 1: case 2:
+                while(opt2!=3){
+                    printf("\nRealizar %s\n", sub[opt1-1]);
+                    printf("\n1 - Produto 1\n2 - Produto 2\n3 - Voltar\n> ");
+                    scanf("%d", &opt2);
+                    getchar();
+                    switch(opt2){
+                        case 1: case 2:
+                            printf("\nProduto %d\n", opt2);
+                            if(opt1==1)
+                                cadastra(&produtos[opt2-1]);
+                            if(opt1==2)
+                                consulta(&produtos[opt2-1]);
+                            opt2=3;
+                            break;
+                        default:
+                            if(opt2!=3)
+                                printf("\nOpcao invalida!\n");
+                            break;
+                    }
+                }
+                break;
+            default:
+                if(opt1!=3)
+                    printf("\nOpcao invalida!\n");
+                break;
+        }
+    }
+    free(produtos);
+    return 0;
 }
